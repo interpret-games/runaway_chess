2d2cda22ba276a0e350ccd336137fe2c5fac5e2f
Hyun Ahn <codepawn@icloud.com>
2024-03-17 21:34:16 +0900

    wip

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,26 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventSheet1_Event5(runtime, localVars)
+	{
+		let now = new Date();
+		
+		let minutes = now.getMinutes();
+		let seconds = now.getSeconds();
+		let milliseconds = now.getMilliseconds();
+		
+		console.log(seconds)
+		console.log(milliseconds)
+	},
+
+	async Battle_sheet_Event8(runtime, localVars)
+	{
+		console.log(RandomEnemyAmount)
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
